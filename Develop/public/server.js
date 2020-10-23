24fa29ab5f532e3d892e9b20441240024533ce54
Decay <devinamccomb@gmail.com>
2020-10-23 01:00:53 -0500

    Updated server

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -14,11 +14,11 @@ const notes = [];
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 })
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/notes.html"));
 })
 
 app.get("/api/notes", (req, res) => {
