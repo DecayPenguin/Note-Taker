77509e50ce8b69c7857bcab316fe06574688a935
Decay <devinamccomb@gmail.com>
2020-10-22 21:41:05 -0500

    Learning

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -10,31 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
-// STORAGE VARIABLES
-// noteTitle, noteText as properties
 const notes = [];
 
-// routing
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
-//GET `/api/notes`
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
-// - Should read the `db.json` file and return all saved notes as JSON
 app.get("/api/notes", (req, res) => {
     const noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
     res.json(noteList);
 })
 
-//POST `/api/notes` - Should receive a new note to save on the request body
-//add it to the `db.json` file, and then return the new note to the client
 app.post("/api/notes", (req, res) => {
+
     var newNote = req.body;
     // add unique ID to note
     // add test functionality to determine if ID already exists, if it does: generate new ID;
@@ -50,10 +44,6 @@ app.post("/api/notes", (req, res) => {
     res.json(noteArr);
 })
 
-//DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to
-//delete. This means you'll need to find a way to give each note a unique `id` when it's
-//saved. In order to delete a note, you'll need to read all notes from the `db.json` file,
-//remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
 app.delete("/api/notes/:id", (req, res) => {
     const { id } = req.params;
     // console.log("Target ID: " + id);
@@ -68,9 +58,10 @@ app.delete("/api/notes/:id", (req, res) => {
             break;
         }
     }
+
     fs.writeFileSync("./db/db.json", JSON.stringify(noteArr), "utf8");
     res.json(noteArr);
 });
 
-// listener for server
+// server listener
 app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
